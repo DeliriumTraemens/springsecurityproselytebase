131e22fdbc2acc28a844b689f529d19cf1f822eb
Grigorich <oskinnick@gmail.com>
2023-07-19 12:39:03 +0300

    Initial commit

---
+++ src/main/java/mykola/springsecurityproselytebase/SpringSecurityProselyteBaseApplication.java
@@ -0,0 +1,13 @@
+package mykola.springsecurityproselytebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityProselyteBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityProselyteBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mykola/springsecurityproselytebase/SpringSecurityProselyteBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package mykola.springsecurityproselytebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityProselyteBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
