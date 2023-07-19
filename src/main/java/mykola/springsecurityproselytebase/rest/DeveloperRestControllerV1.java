caf922a71b776c8fe99a544e94942aa0f2b81b77
Grigorich <oskinnick@gmail.com>
2023-07-19 20:11:58 +0300

    Authorizing methods working

--- src/main/java/mykola/springsecurityproselytebase/rest/DeveloperRestControllerV1.java
+++ src/main/java/mykola/springsecurityproselytebase/rest/DeveloperRestControllerV1.java
@@ -1,6 +1,7 @@
 package mykola.springsecurityproselytebase.rest;
 
 import mykola.springsecurityproselytebase.model.Developer;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,6 +23,7 @@ public class DeveloperRestControllerV1 {
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('developers:read')")
     public Developer getById(@PathVariable Long id) {
         return DEVELOPERS.stream()
                 .filter(developer -> developer.getId().equals(id))
@@ -29,6 +31,7 @@ public class DeveloperRestControllerV1 {
                 .orElse(new Developer(1000L,"User","NotFound"));
     }
     @PostMapping
+    @PreAuthorize("hasAuthority('developers:write')")
     public Developer create(@RequestBody Developer developer){
         this.DEVELOPERS.add(developer);
         return developer;
@@ -40,6 +43,7 @@ public class DeveloperRestControllerV1 {
 //
 //    }
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('developers:write')")
     public void deleteById(@PathVariable Long id) {
         this.DEVELOPERS.removeIf(developer -> developer.getId().equals(id));
     }
